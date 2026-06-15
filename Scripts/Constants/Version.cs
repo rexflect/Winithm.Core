e7b112a222705b85dca6d7afd75d25a605279c40
Nekitori17 <cuongnguyen286641@gmail.com>
2026-06-15 10:41:20 +0700

    ✨ feat(version): add revision field to build version
    - add optional Revision field to BuildVersion record struct
    - initialize Revision to 0 for client and editor versions to ensure compatibility

--- Scripts/Constants/Version.cs
+++ Scripts/Constants/Version.cs
@@ -5,11 +5,12 @@ public static class Version
   public record struct BuildVersion
   {
     public int Major, Minor;
+    public int? Revision;
   }
 
-  public static readonly BuildVersion CLIENT_VERSION = new() { Major = 0, Minor = 1 };
-  public static readonly BuildVersion EDITOR_VERSION = new() { Major = 0, Minor = 1 };
+  public static readonly BuildVersion CLIENT_VERSION = new() { Major = 0, Minor = 1, Revision = 0 };
+  public static readonly BuildVersion EDITOR_VERSION = new() { Major = 0, Minor = 1, Revision = 0 };
 
-  public static readonly BuildVersion SONG_CHART_FORMAT_VERSION = new() { Major = 1, Minor = 3 }; 
-  public static readonly BuildVersion SONG_META_FORMAT_VERSION = new() { Major = 1, Minor = 0 }; 
+  public static readonly BuildVersion SONG_CHART_FORMAT_VERSION = new() { Major = 1, Minor = 3 };
+  public static readonly BuildVersion SONG_META_FORMAT_VERSION = new() { Major = 1, Minor = 0 };
 }
